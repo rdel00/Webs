c73823c08b21cc8ce9cffc25b3edec49c8982d90
RdEl00 <elaslireda35@gmail.com>
2025-02-11 23:03:07 +0100

    Webs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "RdEl00.github.io",
+  base: " ",
   plugins: [svelte()],
 })
